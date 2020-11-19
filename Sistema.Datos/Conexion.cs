74e4b9b48722f75bce35a517391a31a51d0cba19
Daniela Gutierrez <you@example.com>
2020-11-18 22:23:16 -0600

    first commit

---
+++ Sistema.Datos/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool seguridad;
+
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "bd_biblioteca2";
+            this.Servidor = "127.0.0.1";
+            this.Usuario = "root";
+            this.Clave = "";
+            this.seguridad = true;
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.Base + ";";
+                if(this.seguridad == true)
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
+                }
+                else
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User Id="+this.Usuario+";Password="+this.Clave;
+                }
+            }
+            catch(Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if(Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
